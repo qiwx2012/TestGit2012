24beea31ec440664f0b2efff729b0674019abcca
qiwx2012 <476364523@qq.com>
2020-01-26 19:36:16 +0800

    来自Fork项目提交

--- app/src/main/java/t/com/testgit/MainActivity.java
+++ app/src/main/java/t/com/testgit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Fork项目提交代码
+
     }
 }
